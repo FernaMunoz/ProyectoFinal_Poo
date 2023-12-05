b178ed47e9f20b77014a3792acfb1b0d345a3538
mjosepc <mjose.powell@gmail.com>
2023-12-05 20:37:45 -0300

    ventana inicio

---
+++ src/main/java/guis/ventanaInicio.java
@@ -0,0 +1,52 @@
+package guis;
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ventanaInicio extends JFrame implements ActionListener {
+    public ventanaInicio(){
+        super("Inventario");
+        setSize(375,667);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+        JLabel etiqueta1 = new JLabel(new ImageIcon("Captura de pantalla 2023-12-05 181240.png"));
+
+        etiqueta1.setBounds(0,0,375,667);
+
+        JButton boton1 = new JButton("Iniciar sesión");
+        JButton boton2 = new JButton("Registrar");
+        boton1.setBackground(Color.WHITE);
+        boton2.setBackground(Color.WHITE);
+        boton1.setBorder(new LineBorder(Color.WHITE,2,true));
+        boton2.setBorder(new LineBorder(Color.WHITE,2,true));
+
+        boton1.setBounds(87,333,200,50);
+        boton2.setBounds(87,400,200,50);
+
+        boton1.addActionListener(this);
+        boton2.addActionListener(this);
+
+        panel.add(boton1);
+        panel.add(boton2);
+        panel.add(etiqueta1);
+
+        add(panel);
+        setVisible(true);
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("Iniciar sesión")) {
+            ventanaInicioSesion VentanaIniciosesion = new ventanaInicioSesion();
+            VentanaIniciosesion.setVisible(true);
+        } else if(e.getActionCommand().equals("Registrar")) {
+            ventanaRegistro VentanaRegistro = new ventanaRegistro();
+            VentanaRegistro.setVisible(true);
+        }
+    }
+}
