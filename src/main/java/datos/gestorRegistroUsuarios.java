4bd4d9b0d69928165c04d57530a679c9968ff8f3
mjosepc <mjose.powell@gmail.com>
2023-12-08 10:41:24 -0300

    ventana registrar usuario cambiado a csv

--- src/main/java/datos/gestorRegistroUsuarios.java
+++ src/main/java/datos/gestorRegistroUsuarios.java
@@ -1,41 +1,37 @@
 package datos;
 import modelo.Usuario;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.Statement;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class gestorRegistroUsuarios {
     public Usuario usuario;
 
     public Usuario anadirUsuario(String nombre, String nombreEmpresa, String contrasena){
+
         Usuario usuario = null;
-        final String DB_URL = "jdbc:mysql://127.0.0.1:3306/registro_schema";
-        final String Usuario = "root";
-        final String Password = "Valeria32.";
+        String rutaCsv = "registro.csv";
 
-        try{
-            Connection connection = DriverManager.getConnection(DB_URL, Usuario, Password);
-            // conexion a la bases de datos exitoso...
-            Statement estado = connection.createStatement();
-            String sql = "INSERT INTO usuario (nombre, nombreEmpresa, contrasena) " + "VALUES (?,?,?)";
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, nombre);
-            preparedStatement.setString(2,nombreEmpresa);
-            preparedStatement.setString(3,contrasena);
 
-            int filaAnadida = preparedStatement.executeUpdate();
-            if (filaAnadida > 0){
+        try (BufferedWriter escritor = new BufferedWriter(new FileWriter(rutaCsv, true))){
+
+            escritor.write("\"" + nombre + "\",");
+            escritor.write("\"" + nombreEmpresa + "\",");
+            escritor.write("\"" + contrasena + "\"");
+            escritor.newLine();
+
+
+            escritor.flush();
+
                 usuario = new Usuario();
                 usuario.setNombre(nombre);
                 usuario.setNombreEmpresa(nombreEmpresa);
                 usuario.setContrasena(contrasena);
 
                 System.out.println("Registro exitoso");
-            }
 
-        } catch (Exception e){
+        } catch (IOException e){
             e.printStackTrace();
         }
 
